d1213326a5915c1cdba127ec636248c9b00ce2eb
cdlowe3 <69855818+cdlowe3@users.noreply.github.com>
2024-07-31 15:15:14 -0400

    Update

--- Python Scripts/shutdownpc.py
+++ Python Scripts/shutdownpc.py
@@ -1,5 +1,7 @@
 ###Shutdown PCs
 
+import subprocess
+
 Computer_List = ["192.168.68.82"]
 for Computer in Computer_list:
-     print(subprocess.getoutput("shutdown -m \\\\" + Computer +" -f -r -t 0))  
+     print(subprocess.getoutput("shutdown -m \\" + Computer + " -f -r -t 0))
